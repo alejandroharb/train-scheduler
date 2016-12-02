cf328e21983ff32f710c3b21553af5b7da6f020a
alejandro harb <alejandro harb>
2016-12-01 22:41:46 -0600

    click event listener for user submit, storing data into firebase, event listener for new child data

--- assets/app.js
+++ assets/app.js
@@ -7,3 +7,49 @@
     messagingSenderId: "213958702766"
   };
   firebase.initializeApp(config);
+
+  var database = firebase.database();
+
+   //click listener for Submitting a Train Schedule
+   $('#add-train-btn').on('click',function(){
+   		//variables for user input
+   		var trainName = $('#train-submit').val().trim();
+   		var destination = $('#destination-submit').val().trim();
+   		var time = $('#time-submit').val().trim();
+   		var frequency = $('#frequency-submit').val().trim();
+   		//variable temporarily stores user data submitted
+   		var newTrain = {
+   			train: trainName,
+   			destination: destination,
+   			firstTrainTime:time,
+   			frequency:frequency
+   		};
+
+   		//Upload train data
+   		database.ref().push(newTrain);
+
+   		//logs to console
+   		console.log(newTrain.train);
+   		console.log(newTrain.destination);
+   		console.log(newTrain.firstTrainTime);
+   		console.log(newTrain.frequency);
+
+   		//clear all input text boxes after submit
+   		$('#train-submit').val("");
+   		$('#destination-submit').val("");
+   		$('#time-submit').val("");
+   		$('#frequency-submit').val("");
+
+   		//prevents new page load
+   		return false;
+   });
+
+   database.ref().on("child_added", function(response){
+   		console.log(response.val());
+
+   		//store into variables
+   		var train = response.val().trainName;
+   		var destination = response.val().destination;
+   		var time = response.val().time;
+   		var frequency = response.val().frequency;
+   })
